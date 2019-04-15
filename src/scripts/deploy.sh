6394102c000c14969eed53e8a70be1c2a0e4c2c1
Rubens Praser Júnior <praser@gmail.com>
2019-04-15 00:43:46 -0300

    Delegate deploy app install to travis.yml instead of deploy script and add cache to node_modules folder in travis ci

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,17 +1,3 @@
-# install necessary dependency for AWS SDK and ecs-deploy
-sudo add-apt-repository ppa:eugenesan/ppa -y
-sudo apt-get update -y
-sudo apt-get install jq python3-pip -y
-
-# install AWS SDK
-pip3 install --user requests[security] awscli -q
-export PATH=$PATH:$HOME/.local/bin  
-
-# install ecs-deploy
-curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
-  sudo tee -a /usr/bin/ecs-deploy
-sudo chmod +x /usr/bin/ecs-deploy
-
 # login AWS ECR
 eval $(aws ecr get-login --region $AWS_DEFAULT_REGION  --no-include-email | sed 's|https://||')
 
